a90df93995b76aa8e8108b7bb33b41338f8122c1
Kris Melikova <melikova@br-analytics.ru>
2024-05-18 20:06:09 +0300

    Added tests setup part 2

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,22 +1,58 @@
 import asyncio
+from typing import Any
+from urllib.parse import urljoin
 
 import aiohttp
 import pytest
+from redis.asyncio import Redis
+
+from settings import test_settings
 
 
 @pytest.fixture(scope="session")
-async def client_session():
-    return aiohttp.ClientSession
+async def clean_cache():
+    """Удаление всех ключей из Redis (из текущей db)"""
+
+    redis = Redis.from_url(
+        f"redis://{test_settings.redis.host}:{test_settings.redis.port}",
+    )
+    await redis.flushdb()
+    yield redis
+    await redis.close()
 
 
 @pytest.fixture(scope="session")
-def event_loop(request):
-    """ Creates an instance of the default event loop for each test case. """
+async def client_session() -> aiohttp.ClientSession:
+    """AIOHTTP - сессия"""
+
+    client_session = aiohttp.ClientSession()
+    yield client_session
+    await client_session.close()
+
+
+@pytest.fixture(scope="session")
+async def event_loop(request):
+    """Event_loop для scope='session'"""
+
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
-
     loop.close()
 
+
+@pytest.fixture(scope="session")
+async def make_get_request(client_session):
+    """Отправка GET - запроса c AIOHTTP - сессией"""
+
+    async def inner(endpoint: str, params: dict | None = None) -> tuple[Any, Any]:
+        params = params or {}
+        url = urljoin(test_settings.movies_api_url, endpoint)
+        async with client_session.get(url, params=params) as raw_response:
+            response = await raw_response.json()
+            status = raw_response.status
+
+            return status, response
+
+    return inner
 
 # @pytest.fixture
 # def es_write_data():
