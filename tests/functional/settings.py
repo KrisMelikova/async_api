d7e0c3ef4692d2a106120e1d79f8732d56ba28d8
Petr Karasev <petr.karasev@datadvance.net>
2024-05-19 21:23:56 +0300

    Changes for getting enviroment variables

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-from pydantic import Field
 from pydantic_settings import BaseSettings
 
 load_dotenv()
@@ -8,23 +7,23 @@ load_dotenv()
 class RedisSettings(BaseSettings):
     """Конфигурация для Redis"""
 
-    host: str = Field("127.0.0.1", env="REDIS_HOST")
-    port: int = Field(6379, env="REDIS_PORT")
+    REDIS_HOST: str = "127.0.0.1"
+    REDIS_PORT: int = 6379
 
 
 class ElasticSettings(BaseSettings):
     """Конфигурация для ElasticSearch"""
 
-    host: str = Field("127.0.0.1", env="ELASTIC_HOST")
-    port: int = Field(9200, env="ELASTIC_PORT")
+    ELASTIC_HOST: str = "127.0.0.1"
+    ELASTIC_PORT: int = 9200
 
 
 class TestSettings(BaseSettings):
     """Конфигурация проекта для тестирования"""
 
-    movies_api_url: str = "http://127.0.0.1:8000/api/v1/"
-    redis: RedisSettings = RedisSettings()
-    elastic: ElasticSettings = ElasticSettings()
+    MOVIES_API_URL: str = "http://127.0.0.1:8000/api/v1/"
+    REDIS: RedisSettings = RedisSettings()
+    ELASTIC: ElasticSettings = ElasticSettings()
 
 
 test_settings = TestSettings()
