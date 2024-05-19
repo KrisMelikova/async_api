4d6c33175de469867e648454a2f110349f2d93df
Petr Karasev <petr.karasev@datadvance.net>
2024-05-19 03:29:58 +0300

    files for docker were added

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,5 +1,11 @@
+@pytest.fixture(scope='session')
+async def es_client():
+    client = AsyncElasticsearch(hosts='127.0.0.1:9200')
+    yield client
+    await client.close()
+
 @pytest.fixture
-def es_write_data():
+def es_write_data(es_client):
     async def inner(data: List[dict]):
         bulk_query = get_es_bulk_query(data, test_settings.es_index, test_settings.es_id_field)
         str_query = '\n'.join(bulk_query) + '\n'
